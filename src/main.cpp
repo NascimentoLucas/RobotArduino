61d5bc62750bbe00bbce24266bdd6ab580c073e5
NascimentoLucas <nascimentol@outlook.com>
2020-05-03 16:24:54 -0300

    implemented wifi connectiongithub

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,41 @@
-#include <SoftwareSerial.h> //INCLUSÃO DE BIBLIOTECA
+#include "WiFiEsp.h"        //INCLUSÃO DA BIBLIOTECA
+#include "SoftwareSerial.h" //INCLUSÃO DA BIBLIOTECA
+#include "WifiClass.h"
 #include <Arduino.h>
 
-#define SIZE 9
-
-const int pinoRX = 2;    //PINO DIGITAL 2 (RX)
-const int pinoTX = 3;    //PINO DIGITAL 3 (TX)
-const int pinoLed = 13;  //PINO DIGITAL UTILIZADO PELO LED
-int dadoBluetooth = 0;   //VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
-boolean loopLED = false; //VARIÁVEL BOOLEANA QUE FAZ O CONTROLE DE ATIVAÇÃO DO LOOP DO LED
-
-SoftwareSerial bluetooth(pinoRX, pinoTX); //PINOS QUE EMULAM A SERIAL, ONDE
+SoftwareSerial bluetooth(2, 3); //PINOS QUE EMULAM A SERIAL, ONDE
 //O PINO 2 É O RX E O PINO 3 É O TX
+
+SoftwareSerial wifiSerial(6, 7);
+WiFiEspServer server(80); //CONEXÃO REALIZADA NA PORTA 80
+WifiObject wf(wifiSerial, server);
 
 void setup()
 {
-  Serial.begin(9600);    //INICIALIZA A SERIAL
+  Serial.begin(9600); //INICIALIZA A SERIAL
+
+  String ssid = "";
+  String password = "";
+
   bluetooth.begin(9600); //INICIALIZA A SERIAL DO BLUETOOTH
   bluetooth.print("$");  //IMPRIME O CARACTERE
   bluetooth.print("$");  //IMPRIME O CARACTERE
   bluetooth.print("$");  //IMPRIME O CARACTERE
   delay(100);            //INTERVALO DE 100 MILISSEGUNDOS
 
-  String ssid = "";
-  String password = "";
   boolean free = false;
   boolean hasName = false;
 
   Serial.println("Please input SSID: ");
   bluetooth.println("Please input SSID: ");
 
+  int dadoBluetooth = 0; //VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
   do
   {
-
     while (bluetooth.available())
     {
       dadoBluetooth = bluetooth.read();
       char c = dadoBluetooth;
-      Serial.print(c);
       if (c == '\n')
       {
         if (!hasName)
@@ -45,15 +44,18 @@ void setup()
           bluetooth.println(ssid);
           hasName = true;
           bluetooth.println("Please input password");
+          Serial.println("ssid");
           Serial.println(ssid);
-          Serial.println("\n");
+          continue;
         }
         else
         {
           bluetooth.println(password);
           free = true;
+          Serial.println("password");
           bluetooth.println("Connecting");
           Serial.println(password);
+          break;
         }
       }
 
@@ -69,7 +71,31 @@ void setup()
 
   } while (!free);
 
-  bluetooth.print("Start");
+  bluetooth.print("Start Connect");
+  Serial.print("Start Connect");
+  Serial.println(ssid);
+  Serial.println(password);
+
+  char ssidChar[ssid.length() + 1];
+  ssid.toCharArray(ssidChar, ssid.length() + 1);
+
+  char passwordChar[password.length() + 1];
+  password.toCharArray(passwordChar, password.length() + 1);
+
+  wifiSerial.begin(9600);
+  WiFi.init(&wifiSerial); //INICIALIZA A COMUNICAÇÃO SERIAL COM O ESP8266
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    while (true)
+      ;
+  }
+  while (wf.status != WL_CONNECTED)
+  {
+    wf.status = WiFi.begin(ssidChar, passwordChar);
+  }
+  Serial.println("Start Server");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void loop()
