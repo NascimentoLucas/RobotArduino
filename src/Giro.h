40d7b66223b56c0c991e7cdc344f5108d64f5e0b
NascimentoLucas <nascimentol@outlook.com>
2020-05-17 11:16:23 -0300

    implemented bluetooth data send

---
+++ src/Giro.h
@@ -0,0 +1,102 @@
+#include <Wire.h>
+class GiroObject
+{
+    int MPU = 0x68;
+
+public:
+    int minDiff = 0;
+    int acX, acY, acZ, tmp, gyX, gyY, gyZ;
+    int lastAcX, lastAcY, lastAcZ, lastTmp, lastGyX, lastGyY, lastGyZ;
+
+    GiroObject(int newMPU)
+    {
+        /*
+      MPU = newMPU;
+      Wire.begin();
+      Wire.beginTransmission(MPU);
+      Wire.write(0x6B); 
+       
+      //Inicializa o MPU-6050
+      Wire.write(0); 
+      Wire.endTransmission(true);    
+      */
+    }
+
+    bool dif(int last, int actual)
+    {
+        return abs(last - actual) > minDiff;
+    }
+
+    void wiffiUpdate()
+    {
+        update();
+        updateAcel();
+        updateGyro();
+    }
+
+    void update()
+    {
+        Wire.beginTransmission(MPU);
+        Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
+        Wire.endTransmission(false);
+
+        //Solicita os dados do sensor
+        Wire.requestFrom(MPU, 14, true);
+    }
+
+    inline void updateAcel()
+    {
+        acX = Wire.read() << 8 | Wire.read(); //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+        acY = Wire.read() << 8 | Wire.read(); //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+        acZ = Wire.read() << 8 | Wire.read(); //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+        if (dif(lastAcX, acX))
+        {
+            lastAcX = acX;
+        }
+
+        if (dif(lastAcY, acY))
+        {
+            lastAcY = acY;
+        }
+
+        if (dif(lastAcZ, acZ))
+        {
+            lastAcZ = acZ;
+        }
+    }
+
+    void updateGyro()
+    {
+        gyX = Wire.read() << 8 | Wire.read(); //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+        gyY = Wire.read() << 8 | Wire.read(); //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+        gyZ = Wire.read() << 8 | Wire.read(); //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+        if (dif(lastGyX, gyX))
+        {
+            lastGyX = gyX;
+        }
+
+        if (dif(lastGyY, gyY))
+        {
+            lastGyY = gyY;
+        }
+
+        if (dif(lastGyZ, gyZ))
+        {
+            lastGyZ = gyZ;
+        }
+    }
+
+    void updateTemp()
+    {
+        tmp = Wire.read() << 8 | Wire.read(); //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+
+        if (dif(lastTmp, tmp))
+        {
+            Serial.print(" | Temp = ");
+            Serial.println(tmp / 340.00 + 36.53);
+            lastTmp = tmp;
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
+//https://blogmasterwalkershop.com.br/arduino/como-usar-com-arduino-modulo-bluetooth-hc-05-hc-06/
 #include <Arduino.h>
 #include "SoftwareSerial.h"
+#include "Giro.h"
 
 SoftwareSerial bluetooth(2, 3);
 
+#define LED 13
+
 int dadoBluetooth = 0;
+
+bool data = false;
+
+GiroObject go(0x68);
 
 void setup()
 {
   Serial.begin(9600);    //INICIALIZA A SERIAL
   bluetooth.begin(9600); //INICIALIZA A SERIAL DO BLUETOOTH
-  bluetooth.print("$");  //IMPRIME O CARACTERE
-  bluetooth.print("$");  //IMPRIME O CARACTERE
-  bluetooth.print("$");  //IMPRIME O CARACTERE
   delay(100);            //INTERVALO DE 100 MILISSEGUNDOS
 
-    Serial.println("Start");
+  pinMode(LED, OUTPUT);
+
+  Serial.println("Start");
   bluetooth.println("Start");
 }
 
@@ -23,7 +30,38 @@ void loop()
   while (bluetooth.available())
   {
     dadoBluetooth = bluetooth.read();
-    char c = dadoBluetooth;
-    Serial.print(c);
+
+    if (dadoBluetooth == 'O')
+    {
+      data = true;
+    }
+    else if (dadoBluetooth == 'R')
+    {
+      data = false;
+    }
+    else if (dadoBluetooth == 'H')
+    {
+      digitalWrite(LED, HIGH);
+      bluetooth.println("Turn On");
+    }
+    else if (dadoBluetooth == 'L')
+    {
+      digitalWrite(LED, LOW);
+      bluetooth.println("Turn Off");
+    }
+  }
+
+  if (data)
+  {
+    data = false;
+    String s = "";
+    s = s + "lastAcX=" + go.lastAcX + ";";
+    s = s + "lastAcY=" + go.lastAcY + ";";
+    s = s + "lastAcZ=" + go.lastAcZ + ";";
+    s = s + "lastGyX=" + go.lastGyX + ";";
+    s = s + "lastGyY=" + go.lastGyY + ";";
+    s = s + "lastGyZ=" + go.lastGyZ + ";";
+
+    bluetooth.println(s);
   }
 }
